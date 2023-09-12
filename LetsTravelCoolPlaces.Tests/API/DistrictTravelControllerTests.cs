d1fd1a3ca0beb9e449149ee395390d97f4d944b3
Rahat Hossain <mdrhmoon@gmail.com>
2023-09-13 00:49:37 +0600

    Task 19: Unit Test Add Unit test for DistrictTravelController

--- LetsTravelCoolPlaces.Tests/API/DistrictTravelControllerTests.cs
+++ LetsTravelCoolPlaces.Tests/API/DistrictTravelControllerTests.cs
@@ -2,21 +2,104 @@
 
 public class DistrictTravelControllerTests
 {
+    private readonly DistrictTravelController sut = null;
+    private readonly IDistrictService districtService = null;
+    private readonly ITemperatureService temperatureService = null;
+
     public DistrictTravelControllerTests()
     {
-        
+        districtService = Substitute.For<IDistrictService>();
+        temperatureService = Substitute.For<ITemperatureService>();
+
+        sut = new DistrictTravelController(temperatureService, districtService);
     }
 
     [Fact]
-    public async Task GetDistricts_ShouldHave_OkStatus()
+    public async Task GetDistricts_ShouldReturnOkResponse()
     {
         // Arrange
-        var con = new DistrictTravelController();
-
         // Act
-        var result = await con.GetDistricts();
+        var result = await sut.GetDistricts();
 
         // Assert
-        Assert.Equal(((OkObjectResult)result).StatusCode, 200);
+        ((OkObjectResult)result).StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task GetDistricts_ShouldReturnListOfDistricts()
+    {
+        // Arrange
+        var mockData = new DistrictMock().GetDistrict();
+        districtService.GetDistrictsFromApi().Returns(mockData);
+
+        // Act
+        var result = await sut.GetDistricts();
+
+        // Assert
+        ((OkObjectResult)result).StatusCode.Should().Be(200);
+        ((OkObjectResult)result).Value.Should().NotBeNull();
+        ((IList<District>)((OkObjectResult)result).Value).Should().HaveCount(c => c >= 2);
+    }
+
+    [Fact]
+    public async Task GetCooleDistricts_ShouldReturnOkResponse()
+    {
+        // Arrange
+        // Act
+        var result = await sut.GetCoolestDistricts();
+
+        // Assert
+        ((OkObjectResult)result).StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task GetCooleDistricts_ListOfCoolestDistrict()
+    {
+        // Arrange
+        var mockData = new DistrictMock().GetDistrict();
+        temperatureService.GetCoolestDistricts().Returns(mockData);
+        // Act
+        var result = await sut.GetCoolestDistricts();
+
+        // Assert
+        ((OkObjectResult)result).StatusCode.Should().Be(200);
+        ((OkObjectResult)result).Value.Should().NotBeNull();
+        ((IList<District>)((OkObjectResult)result).Value).Should().HaveCount(c => c >= 2);
+    }
+
+    [Theory]
+    [InlineData("1", "7", "2023-09-14", "Can Travel")]
+    [InlineData("7", "1", "2023-09-15", "Can Travel")]
+    [InlineData("7", "3", "2023-09-16", "Can Travel")]
+    [InlineData("7", "3", "2023-09-21", "Can't Travel")]
+    public async Task GetTravelPossibility_ShouldReturnOkResponse(string currentId, string destinationId, string date, string expectedResult)
+    {
+        // Arrange
+        temperatureService.GetTravelPossibility(currentId, destinationId, date).Returns(expectedResult);
+        // Act
+        var result = await sut.GetTravelPossibility(currentId, destinationId, date);
+
+        // Assert
+        ((OkObjectResult)result).StatusCode.Should().Be(200);
+        ((OkObjectResult)result).Value.Should().NotBeNull();
+        ((string)((OkObjectResult)result).Value).Should().Match(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("1", "7", "2023-09-14", "Can Travel")]
+    [InlineData("7", "1", "2023-09-15", "Can Travel")]
+    [InlineData("7", "3", "2023-09-16", "Can Travel")]
+    [InlineData("7", "3", "2023-09-21", "Can't Travel")]
+    public async Task GetTravelPossibility_ShouldReturnCannotTravel(string currentId, string destinationId, string date, string expectedResult)
+    {
+        // Arrange
+        temperatureService.GetTravelPossibility(currentId, destinationId, date).Returns(expectedResult);
+        // Act
+        var result = await sut.GetTravelPossibility(currentId, destinationId, date);
+
+        // Assert
+        ((OkObjectResult)result).StatusCode.Should().Be(200);
+        ((OkObjectResult)result).Value.Should().NotBeNull();
+        ((string)((OkObjectResult)result).Value).Should().Match(expectedResult);
     }
 }
--- LetsTravelCoolPlaces.Tests/GlobalUsings.cs
+++ LetsTravelCoolPlaces.Tests/GlobalUsings.cs
@@ -1,6 +1,12 @@
-global using Xunit;
-global using System.Linq;
-global using Microsoft.AspNetCore.Mvc;
+global using FluentAssertions;
 global using LetsTravelCoolPlaces.API.Controllers;
-global using LetsTravelCoolPlaces.Services;
 global using LetsTravelCoolPlaces.Core.Models;
+global using LetsTravelCoolPlaces.Services;
+global using Newtonsoft.Json;
+global using NSubstitute;
+global using System.Linq;
+global using Xunit;
+global using LetsTravelCoolPlaces.Tests.MockData;
+global using Microsoft.AspNetCore.Mvc;
+
+
