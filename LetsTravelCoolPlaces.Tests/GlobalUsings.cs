91d26dc6881e896d946ba45d0648b2ebd3552322
Rahat Hossain <mdrhmoon@gmail.com>
2023-09-12 18:40:08 +0600

    Task 09: Add test for district service GetDistrictsFromApi method

--- LetsTravelCoolPlaces.Tests/GlobalUsings.cs
+++ LetsTravelCoolPlaces.Tests/GlobalUsings.cs
@@ -1,3 +1,6 @@
 global using Xunit;
+global using System.Linq;
 global using Microsoft.AspNetCore.Mvc;
 global using LetsTravelCoolPlaces.API.Controllers;
+global using LetsTravelCoolPlaces.Services;
+global using LetsTravelCoolPlaces.Core.Models;
---
+++ LetsTravelCoolPlaces.Tests/Service/DistrictServiceTests.cs
@@ -0,0 +1,22 @@
+﻿namespace LetsTravelCoolPlaces.Tests.Service;
+
+public class DistrictServiceTests
+{
+    public DistrictServiceTests()
+    {
+        
+    }
+
+    [Fact]
+    public async Task GetDistricts_ShouldHave_DistrictsList()
+    {
+        // Arrange
+        var service = new DistrictService();
+
+        // Act
+        var result = await service.GetDistrictsFromApi();
+
+        // Assert
+        Assert.Equal("1", result?.Where(x => x.Id == "1").Select(x => x.Id).First());
+    }
+}
